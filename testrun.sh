a63a92d94ea2a867d02faf64d5ed570f8fe1919a
NischayDiwan <cs5200433@iitd.ac.in>
2023-04-15 23:46:41 +0530

    submission

--- testrun.sh
+++ testrun.sh
@@ -1,13 +1,13 @@
 make clean
 make 
-for i in 2 8 16
+for i in 2
 do
-    for j in 0
+    for j in 25
     do
         echo "K: $i"
         echo "NUM_MERGES: $j"
         make remove
+        # rm output.txt
         ./exec ../random.txt output.txt 1000000 $i $j
-        diff output.txt ../randomexp.txt
     done
 done
